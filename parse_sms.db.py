561ea9dd1df71299395f288124691131b3eb0549
Albert Hui <i@hack9u.online>
2025-01-02 21:37:56 +0800

    Added read time; changed empty string display to be no cell value; improved command line arguments parsing

--- parse_sms.db.py
+++ parse_sms.db.py
@@ -5,13 +5,14 @@ parse_sms.db.py - Parse sms.db from iOS, supports edited and unsent messages
 
 Author: Albert Hui <albert@securityronin.com>
 """
-__updated__ = '2024-12-31 23:37:44'
+__updated__ = '2025-01-02 21:35:06'
 
-import argparse
+from typing import Dict, List
+from argparse import ArgumentParser, Namespace
 from pathlib import Path
+import sys
 import sqlite3 
-from datetime import datetime
-import pytz
+from datetime import datetime, timezone
 import plistlib
 import typedstream
 
@@ -24,7 +25,7 @@ def macAbsTimeToUnixTime(macAbsoluteTime):
 	return macAbsoluteTime + 978307200
 
 def unixTimeToString(unixTime):
-	return datetime.fromtimestamp(unixTime,pytz.timezone('UTC')).strftime('%Y-%m-%d %H:%M:%S UTC')
+	return datetime.fromtimestamp(unixTime, tz=timezone.utc).strftime('%Y-%m-%d %H:%M:%S UTC')
 
 def openSQLiteDB(db):
 	try:
@@ -34,64 +35,72 @@ def openSQLiteDB(db):
 		print(f"Error opening sms.db file: {e}")
 		return None
 
-parser = argparse.ArgumentParser()
-parser.add_argument("file", help="sms.db file, usually located in /private/var/mobile/Library/SMS/sms.db")
-args = parser.parse_args()
-smsdbfile = Path(args.file)
+def parseArgs(cliArgs: List[str] = None) -> Namespace: # None means sys.argv[1:]
+	parser = ArgumentParser()
+	parser.add_argument("file", help="sms.db file, usually located in /private/var/mobile/Library/SMS/sms.db")
+	return parser.parse_args(args=cliArgs)
 
-if not smsdbfile.is_file():
-    print("The sms.db file doesn't exist")
-    raise SystemExit(1)
+def main(args: Namespace = parseArgs()) -> int:
+	smsdbfile = Path(args.file)
+	if not smsdbfile.is_file():
+		print("The sms.db file doesn't exist")
+		raise SystemExit(1)
 
-conn = openSQLiteDB(smsdbfile)
-conn.row_factory = sqlite3.Row
-c = conn.cursor() 
-statement = '''SELECT * FROM message m, handle h WHERE m.handle_id = h.ROWID ORDER BY m.ROWID'''
-c.execute(statement) 
+	conn = openSQLiteDB(smsdbfile)
+	conn.row_factory = sqlite3.Row
+	c = conn.cursor() 
+	statement = '''SELECT * FROM message m, handle h WHERE m.handle_id = h.ROWID ORDER BY m.ROWID'''
+	c.execute(statement) 
 
-print("ROWID,From/To,Counterparty,Service,Original Time,Original Text,Edited Time,Edited Text")
-for row in c.fetchall():
-	ROWID = row['ROWID']
-	fromto = "To" if row['is_from_me'] == 1 else "From"
-	id = row['id'] # handle.id
-	service = row['service']
-	date = unixTimeToString(macAbsTimeToUnixTime(row['date']))
-	text = f"'{row['text']}'" if row['text'] is not None else ''
-	date_edited = unixTimeToString(macAbsTimeToUnixTime(row['date_edited'])) if row['date_edited'] else ''
-	text_edited = "''"
+	print("ROWID,From/To,Counterparty,Service,Sent/Scheduled Time,Text,Read Time,Edited Time,Edited Text")
+	for row in c.fetchall():
+		ROWID = row['ROWID']
+		fromto = "To" if row['is_from_me'] == 1 else "From"
+		id = row['id'] # handle.id
+		service = row['service']
+		date = unixTimeToString(macAbsTimeToUnixTime(row['date']))
+		text = f"'{row['text']}'" if row['text'] is not None else ''
+		date_read = unixTimeToString(macAbsTimeToUnixTime(row['date_read'])) if row['date_read'] else '[not read]'
+		date_edited = unixTimeToString(macAbsTimeToUnixTime(row['date_edited'])) if row['date_edited'] else ''
+		text_edited = ''
 
-	if date_edited != '' and text == '':
-		# edited message with no original text
-		text = '[cleared upon unsent]'
-		text_edited = '[unsent]'
+		if date_edited != '' and text == '':
+			# edited message with no original text
+			text = '[cleared upon unsent]'
+			text_edited = '[unsent]'
 
-	# parse original and edited texts from message_summary_info
-	if row['message_summary_info'] is None:
-		continue
-	message_summary_info = plistlib.loads(row['message_summary_info'])
-	if 'ec' in message_summary_info and '0' in message_summary_info['ec']:
-		# original text
-		ts = typedstream.unarchive_from_data((((message_summary_info['ec'])['0'])[0])['t'])
-		for c in ts.contents:
-			for v in c.values:
-				# check if v has the property 'archived_name'
-				if not (hasattr(v, 'archived_name') and hasattr(v, 'value')):
-					continue
-				if (v.archived_name == b'NSMutableString' or v.archived_name == b'NSString') and v.value is not None:
-					text = f"'{v.value}'"
-					break
-		# edited text
-		ts = typedstream.unarchive_from_data((((message_summary_info['ec'])['0'])[1])['t'])
-		for c in ts.contents:
-			for v in c.values:
-				# check if v has the property 'archived_name'
-				if not (hasattr(v, 'archived_name') and hasattr(v, 'value')):
-					continue
-				if (v.archived_name == b'NSMutableString' or v.archived_name == b'NSString') and v.value is not None:
-					text_edited = f"'{v.value}'"
-					break
+		# parse original and edited texts from message_summary_info
+		if row['message_summary_info'] is None:
+			continue
+		message_summary_info = plistlib.loads(row['message_summary_info'])
+		if 'ec' in message_summary_info and '0' in message_summary_info['ec']:
+			# original text
+			ts = typedstream.unarchive_from_data((((message_summary_info['ec'])['0'])[0])['t'])
+			for c in ts.contents:
+				for v in c.values:
+					# check if v has the property 'archived_name'
+					if not (hasattr(v, 'archived_name') and hasattr(v, 'value')):
+						continue
+					if (v.archived_name == b'NSMutableString' or v.archived_name == b'NSString') and v.value is not None:
+						text = f"'{v.value}'"
+						break
+			# edited text
+			ts = typedstream.unarchive_from_data((((message_summary_info['ec'])['0'])[1])['t'])
+			for c in ts.contents:
+				for v in c.values:
+					# check if v has the property 'archived_name'
+					if not (hasattr(v, 'archived_name') and hasattr(v, 'value')):
+						continue
+					if (v.archived_name == b'NSMutableString' or v.archived_name == b'NSString') and v.value is not None:
+						text_edited = f"'{v.value}'"
+						break
 
-	print(f"{ROWID},{fromto},'{id}',{service},{date},{text},{date_edited},{text_edited}")
+		print(f"{ROWID},{fromto},'{id}',{service},{date},{text},{date_read},{date_edited},{text_edited}")
 
-conn.commit()
-conn.close()
+	conn.commit()
+	conn.close()
+
+	return 0 # success
+
+if __name__ == "__main__":
+    sys.exit(main(parseArgs()))
